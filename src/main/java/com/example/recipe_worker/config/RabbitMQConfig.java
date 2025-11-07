c3f4a352a9884c797b171360c7aa86d795a71767
archis1405 <archismandas1405@gmail.com>
2025-11-08 02:59:07 +0530

    Added the RabbitMQ config

---
+++ src/main/java/com/example/recipe_worker/config/RabbitMQConfig.java
@@ -0,0 +1,64 @@
+package com.example.recipe_worker.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.queue}")
+    private String queueName;
+
+    @Value("${rabbitmq.exchange}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing-key}")
+    private String routingKey;
+
+
+    @Bean
+    public Queue recipeQueue() {
+        return QueueBuilder.durable(queueName)
+                .withArgument("x-dead-letter-exchange", exchange + ".dlx")
+                .build();
+    }
+
+
+    @Bean
+    public TopicExchange recipeExchange() {
+        return new TopicExchange(exchange);
+    }
+
+
+    @Bean
+    public Binding binding(Queue recipeQueue, TopicExchange recipeExchange) {
+        return BindingBuilder
+                .bind(recipeQueue)
+                .to(recipeExchange)
+                .with(routingKey);
+    }
+
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(messageConverter());
+        factory.setConcurrentConsumers(3);
+        factory.setMaxConcurrentConsumers(10);
+        factory.setPrefetchCount(5);
+        return factory;
+    }
+}
